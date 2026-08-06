42eeba8f5cc89c7e9d0bff71698b2d3746caf698
kaioumdev <afrankaiyum34@gmail.com>
2026-08-07 02:47:43 +0600

    make socket function to connect and using the socket

---
+++ socket.js
@@ -0,0 +1,74 @@
+const socketIo = require('socket.io');
+const userModel = require('./models/user.model');
+const captainModel = require('./models/captain.model');
+
+let io;
+
+function initializeSocket(server) {
+    io = socketIo(server, {
+        cors: {
+            origin: '*',
+            methods: [ 'GET', 'POST' ]
+        }
+    });
+
+    io.on('connection', (socket) => {
+        console.log(`Client connected: ${socket.id}`);
+
+
+        socket.on('join', async (data) => {
+            const { userId, userType } = data;
+
+            if (userType === 'user') {
+                await userModel.findByIdAndUpdate(userId, { socketId: socket.id });
+            } else if (userType === 'captain') {
+                await captainModel.findByIdAndUpdate(userId, { socketId: socket.id });
+            }
+        });
+
+
+        socket.on('update-location-captain', async (data) => {
+            const { userId, location } = data;
+
+            if (!location || !location.ltd || !location.lng) {
+                return socket.emit('error', { message: 'Invalid location data' });
+            }
+
+            await captainModel.findByIdAndUpdate(userId, {
+                location: {
+                    ltd: location.ltd,
+                    lng: location.lng
+                }
+            });
+
+            // Broadcast captain's live location to the user during an active ride
+            const captain = await captainModel.findById(userId);
+            if (captain && captain.currentRideUserId) {
+                const user = await userModel.findById(captain.currentRideUserId);
+                if (user && user.socketId) {
+                    io.to(user.socketId).emit('captain-location-update', {
+                        ltd: location.ltd,
+                        lng: location.lng
+                    });
+                }
+            }
+        });
+
+        socket.on('disconnect', () => {
+            console.log(`Client disconnected: ${socket.id}`);
+        });
+    });
+}
+
+const sendMessageToSocketId = (socketId, messageObject) => {
+
+console.log(messageObject);
+
+    if (io) {
+        io.to(socketId).emit(messageObject.event, messageObject.data);
+    } else {
+        console.log('Socket.io not initialized.');
+    }
+}
+
+module.exports = { initializeSocket, sendMessageToSocketId };
