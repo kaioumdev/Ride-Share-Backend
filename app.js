1f5395408e1391e83d1a98c7adf3ef2f6e724be5
kaioumdev <afrankaiyum34@gmail.com>
2026-08-08 13:03:05 +0600

    add client live link to the backend server

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ connectToDb();
 
 // app.use(cors());
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175'],
+  origin: [ 'https://ride-share-frontend-zeta.vercel.app', 'http://localhost:5173'],
 }))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
